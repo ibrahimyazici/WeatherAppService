e5f36b2aa0e4cb930d4f9534ce35689e609470eb
ibrhmyzc <ibrhmyazici@gmail.com>
2020-01-05 02:58:23 +0300

    master user is added

--- weatherforecastdemo/src/main/java/com/ibrahim/weatherforecastdemo/authentication/AuthenticationController.java
+++ weatherforecastdemo/src/main/java/com/ibrahim/weatherforecastdemo/authentication/AuthenticationController.java
@@ -1,11 +1,14 @@
 package com.ibrahim.weatherforecastdemo.authentication;
 
+import com.ibrahim.weatherforecastdemo.models.User;
+import com.ibrahim.weatherforecastdemo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -16,12 +19,28 @@ public class AuthenticationController {
     @Autowired
     private MyUserDetailsService userDetailsService;
     @Autowired
+    private UserService userService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
     private JwtUtil jwtTokenUtil;
     @RequestMapping(value = "/authentication", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(
             @RequestBody AuthenticationRequest authenticationRequest
     ) throws Exception {
         try {
+            // THIS PART IS ONLY FOR DEMO PURPOSES
+            // in order to create users, we need to log in that is why
+            // I am adding this master username and password
+            // So you can log in and check the features
+            if(authenticationRequest.getUsername().equals("etiya")
+                && authenticationRequest.getPassword().equals("etiya")){
+                User user = new User();
+                user.setUsername(authenticationRequest.getUsername());
+                user.setPassword(bCryptPasswordEncoder.encode(authenticationRequest.getPassword()));
+                user.setTitle("admin");
+                userService.save(user);
+            }
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             authenticationRequest.getUsername(), authenticationRequest.getPassword()
--- weatherforecastdemo/src/main/java/com/ibrahim/weatherforecastdemo/models/User.java
+++ weatherforecastdemo/src/main/java/com/ibrahim/weatherforecastdemo/models/User.java
@@ -14,6 +14,7 @@ public class User {
     private String password;
     private String title;
 
+
     public long getId() {
         return id;
     }
